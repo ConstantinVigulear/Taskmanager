5032be2b60dccc840377aaf45bc1e09b262c351c
ConstantinVigulear <71497876+ConstantinVigulear@users.noreply.github.com>
2021-05-06 14:59:42 +0300

    Delete taskmanager directory

--- taskmanager/model-dao-service-factory-util/src/main/java/com/stefanini/taskmanager/commands/Command.java
+++
@@ -1,7 +0,0 @@
-package com.stefanini.taskmanager.commands;
-
-public interface Command {
-
-	void execute() throws Exception;
-	void unexecute();
-}
--- taskmanager/model-dao-service-factory-util/src/main/java/com/stefanini/taskmanager/factories/DAOFactory.java
+++
@@ -1,6 +0,0 @@
-package com.stefanini.taskmanager.factories;
-
-// Interface for concrete DAO factories.
-public interface DAOFactory<T> {
-	public T getDAO();
-}
